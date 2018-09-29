88893fb8b54c25ecbec17ca784019800e8a63639
DESKTOP-73HHDDA\mako9890 <mako98901993@hotmail.com>
2018-09-29 13:14:43 -0400

    Added script stubs for base architecture.

---
+++ SyncronizeWatches/Assets/Scripts/EventBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
