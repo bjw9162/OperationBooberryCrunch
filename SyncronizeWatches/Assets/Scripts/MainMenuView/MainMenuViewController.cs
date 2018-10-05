59b471cd6384d290d0a2e8497def50a9be03489d
Robert Pruden <rdp1070@rit.edu>
2018-10-04 21:26:14 -0400

    Add Basic Main Menu

---
+++ SyncronizeWatches/Assets/Scripts/MainMenuView/MainMenuViewController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuViewController : MonoBehaviour
+{
+
+    public GameObject TitleText;
+    public List<Button> buttons;
+    public string gameScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttons[0].onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // use this to initialize a game session, and then send over to the scene.
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+}
